fbbbe4b1019c5d07ff833dd46ec0a6f455ff4738
CAIO QUEIROZ CAETANO <caio.caetano@a.ucb.br>
2026-08-20 21:40:46 -0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style='inverted'/>
+
+
+      <Text style={styles.title}>Me deixa quieto, eu não quero mexer com front-end!</Text>
+
+      <View style={styles.main}>
+        <Image source={{ uri: "https://github.com/CaioQ.png" }} style={styles.foto}>
+        </Image>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexGrow: 1,
+    backgroundColor: '#202024',
+    alignItems: 'center'
   },
+  title: {
+    fontSize: 40,
+    color: "#fff"
+  },
+  main: {
+    flex: 1, //ocupa todo o espaço disponível
+    width: "100%",
+    backgroundColor: "#121214"
+  },
+  avatar: {
+    width: 148,
+    height: 148,
+    borderRadius: 148,
+    borderWidth: 1,
+    borderColor: "#ccc"
+  }
 });
